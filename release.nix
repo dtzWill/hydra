c640849b77e58b1fb2f4a8b7284d8e80c9d0d717
Will Dietz <w@wdtz.org>
2018-05-22 18:42:05 -0500

    release: point at branch on my repo

--- release.nix
+++ release.nix
@@ -3,7 +3,7 @@
 , shell ? false
 # , nixpkgs ? <nixpkgs>
 # XXX: this isn't plumbed through hydra-module.nix, oh well
-, nixpkgs ? builtins.fetchGit { url = https://github.com/NixOS/nixpkgs; rev = "29bb31bd25a45ebe8a3500c4402665e89743b394"; }
+, nixpkgs ? builtins.fetchGit { url = https://github.com/dtzWill/nixpkgs; ref = "update/nixUnstable-6208"; }
 }:
 
 let
